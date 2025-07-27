ff7abc9b0f9ccb969b09fd59a78530f60d665708
Gedion Fetene <gedionfetene77@gmail.com>
2025-07-27 22:01:47 +0300

    Import Montserrat font and update homepage

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,15 +1,9 @@
-// pages/index.tsx
-import Layout from "@/components/layouts/Layout";
-
-export default function Home() {
+const Home = () => {
   return (
-    <Layout>
-      <section className="min-h-screen flex flex-col items-center justify-center">
-        <h1 className="text-4xl font-bold text-gray-800 mb-4">
-          Welcome to Splash App
-        </h1>
-        <p className="text-lg text-gray-600">This is your home page content!</p>
-      </section>
-    </Layout>
+    <div>
+      <h1> Welcome to Splash App </h1>
+    </div>
   );
-}
+};
+
+export default Home;
